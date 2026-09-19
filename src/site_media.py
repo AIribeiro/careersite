78c0920434d26f7e9eea9b1b4fdc59d27f741e93
AIribeiro <97567343+AIribeiro@users.noreply.github.com>
2026-09-19 17:20:26 +0200

    Expose EITCA EU banner asset

--- src/site_media.py
+++ src/site_media.py
@@ -37,6 +37,7 @@ brand_icon = contact
 presence_thinkers360 = _uri("presence_thinkers360_top50.png")
 presence_global_ambassador = _uri("presence_global_ai_ambassador.jpg")
 presence_ai_learning = _uri("presence_ai_learning_sessions.jpg")
+eitca_eu_banner = _uri("eitca_eu_banner.webp")
 
 if hero:
     assets.HERO_URI = hero
